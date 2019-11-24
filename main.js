13f73b1d86d17ba2a6b6f9ea4d71fa0306732f2e
oyuioy <hgjhgj@yahoo.com>
2019-11-24 17:09:41 +0200

    finishing-js

--- main.js
+++ main.js
@@ -15,3 +15,44 @@ function insertNum(num) {
     }
     if (operator) decimal = false;
 }
+
+function insertOp (op) {
+    textview.value = exp + op;
+    operator = true;
+    equal = false;
+    allowSR = false;
+}
+
+function insertDec () {
+    if (number && !decimal) {
+        textview.value = exp + '.';
+        decimal = true;
+        operator = false;
+    }
+}
+
+function equalTo() {
+    if (exp) {
+        exp = eval(exp);
+        textview.value = exp;
+        equal = true;
+        decimal = false;
+        number = false;
+        allowSR = true;
+    }
+}
+
+function clean() {
+    exp = '';
+    textview.value = exp;
+    decimal = false;
+}
+
+function back() {
+    exp = textview.value;
+    exp = exp.substring(0, exp.length - 1);
+    textview.value = exp;
+}
+
+
+
