6488cdcf69c43e31bae301e1511516be747f53b8
oyuioy <hgjhgj@yahoo.com>
2019-11-24 16:22:44 +0200

    some-js-done

---
+++ main.js
@@ -0,0 +1,17 @@
+var exp = '', number, decimal, equal, operator, allowSR = true;
+var textview = document.forms['myForm']['textview'];
+
+function insertNum(num) {
+    if (equal) {
+        exp = num;
+        textview.value = exp;
+        number = true;
+        equal = false;
+    }
+    else {
+        exp = textview.value + num;
+        textview.value = exp;
+        number = true;
+    }
+    if (operator) decimal = false;
+}
